304619ca4164de724d9194f7f42afb6a16d9577a
Som Sahu <sosahu@microsoft.com>
2017-09-12 14:47:00 -0700

    remove unused imports

--- src/test/java/com/microsoft/azure/eventhubs/kafka/connect/sink/EventHubSinkTaskTest.java
+++ src/test/java/com/microsoft/azure/eventhubs/kafka/connect/sink/EventHubSinkTaskTest.java
@@ -6,7 +6,6 @@ import org.apache.kafka.connect.sink.SinkRecord;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.runners.MockitoJUnitRunner;
 
@@ -15,7 +14,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.LinkedBlockingQueue;
 
 import static org.mockito.Mockito.*;
 
